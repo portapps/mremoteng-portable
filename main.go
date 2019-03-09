ee051660d226f153c23876f195a9b7619ae75707
CrazyMax <crazy-max@users.noreply.github.com>
2019-03-09 21:05:26 +0100

    Portapps 1.20.2

--- main.go
+++ main.go
@@ -3,98 +3,110 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
 	. "github.com/portapps/portapps"
+	"github.com/portapps/portapps/pkg/utl"
+)
+
+var (
+	app *App
 )
 
 func init() {
-	Papp.ID = "mremoteng-portable"
-	Papp.Name = "mRemoteNG"
-	Init()
+	var err error
+
+	// Init app
+	if app, err = New("mremoteng-portable", "mRemoteNG"); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
 }
 
 func main() {
-	Papp.AppPath = AppPathJoin("app")
-	Papp.DataPath = CreateFolder(AppPathJoin("data"))
-	Papp.Process = PathJoin(Papp.AppPath, "mRemoteNG.exe")
-	Papp.Args = []string{"/cons:" + FormatWindowsPath(PathJoin(Papp.DataPath, "confCons.xml"))}
-	Papp.WorkingDir = Papp.AppPath
+	utl.CreateFolder(app.DataPath)
+	app.Process = utl.PathJoin(app.AppPath, "mRemoteNG.exe")
+	app.Args = []string{
+		fmt.Sprintf("/cons:%s", utl.FormatWindowsPath(utl.PathJoin(app.DataPath, "confCons.xml"))),
+	}
 
 	// mRemoteNG.settings check
-	dSettings := PathJoin(Papp.DataPath, "mRemoteNG.settings")
-	aSettings := PathJoin(Papp.AppPath, "mRemoteNG.settings")
-	if !Exists(dSettings) {
-		if err := WriteToFile(dSettings, `<?xml version="1.0" encoding="utf-8"?><settings/>`); err != nil {
-			Log.Error("Cannot write to mRemoteNG.settings: ", err)
+	dSettings := utl.PathJoin(app.DataPath, "mRemoteNG.settings")
+	aSettings := utl.PathJoin(app.AppPath, "mRemoteNG.settings")
+	if !utl.Exists(dSettings) {
+		if err := utl.WriteToFile(dSettings, `<?xml version="1.0" encoding="utf-8"?><settings/>`); err != nil {
+			Log.Fatal().Err(err).Msg("Cannot write to mRemoteNG.settings")
 		}
 	}
-	os.Remove(aSettings)
+	_ = os.Remove(aSettings)
 	if err := os.Symlink(dSettings, aSettings); err != nil {
-		Log.Error("Cannot create symlink to mRemoteNG.settings: ", err)
+		Log.Fatal().Err(err).Msg("Cannot create symlink to mRemoteNG.settings")
 	}
 
 	// extApps.xml exists in data ? Create symlink and remove old one
-	dExtApps := PathJoin(Papp.DataPath, "extApps.xml")
-	aExtApps := PathJoin(Papp.AppPath, "extApps.xml")
-	if Exists(dExtApps) {
-		os.Remove(aExtApps)
+	dExtApps := utl.PathJoin(app.DataPath, "extApps.xml")
+	aExtApps := utl.PathJoin(app.AppPath, "extApps.xml")
+	if utl.Exists(dExtApps) {
+		_ = os.Remove(aExtApps)
 		if err := os.Symlink(dExtApps, aExtApps); err != nil {
-			Log.Error("Cannot create symlink to extApps.xml: ", err)
+			Log.Fatal().Err(err).Msg("Cannot create symlink to extApps.xml")
 		}
 	}
 
 	// pnlLayout.xml exists in data ? Create symlink and remove old one
-	dPnlLayout := PathJoin(Papp.DataPath, "pnlLayout.xml")
-	aPnlLayout := PathJoin(Papp.AppPath, "pnlLayout.xml")
-	if Exists(dPnlLayout) {
-		os.Remove(aPnlLayout)
+	dPnlLayout := utl.PathJoin(app.DataPath, "pnlLayout.xml")
+	aPnlLayout := utl.PathJoin(app.AppPath, "pnlLayout.xml")
+	if utl.Exists(dPnlLayout) {
+		_ = os.Remove(aPnlLayout)
 		if err := os.Symlink(dPnlLayout, aPnlLayout); err != nil {
-			Log.Error("Cannot create symlink to pnlLayout.xml: ", err)
+			Log.Fatal().Err(err).Msg("Cannot create symlink to pnlLayout.xml")
 		}
 	}
 
-	Launch(os.Args[1:])
+	// On exit
+	defer func() {
+		// confCons.xml copy back on close if not exists in data
+		dConfCons := utl.PathJoin(app.DataPath, "confCons.xml")
+		aConfCons := utl.PathJoin(app.AppPath, "confCons.xml")
+		if !utl.Exists(dConfCons) && utl.Exists(aConfCons) {
+			if err := utl.CopyFile(aConfCons, dConfCons); err != nil {
+				Log.Error().Err(err).Msg("Cannot copy confCons.xml")
+			}
+			_ = os.Remove(aConfCons)
+			if err := os.Symlink(dConfCons, aConfCons); err != nil {
+				Log.Error().Err(err).Msg("Cannot create symlink to confCons.xml")
+			}
+		}
+		oldConfConsFiles, _ := filepath.Glob(utl.PathJoin(app.AppPath, "confCons*"))
+		for _, oldConfConsFile := range oldConfConsFiles {
+			if err := os.Remove(oldConfConsFile); err != nil {
+				Log.Error().Err(err).Msg("Cannot remove old confCons file")
+			}
+		}
 
-	// confCons.xml copy back on close if not exists in data
-	dConfCons := PathJoin(Papp.DataPath, "confCons.xml")
-	aConfCons := PathJoin(Papp.AppPath, "confCons.xml")
-	if !Exists(dConfCons) && Exists(aConfCons) {
-		if err := CopyFile(aConfCons, dConfCons); err != nil {
-			Log.Error("Cannot copy confCons.xml: ", err)
+		// extApps.xml handling on close
+		if !utl.Exists(dExtApps) && utl.Exists(aExtApps) {
+			if err := utl.CopyFile(aExtApps, dExtApps); err != nil {
+				Log.Error().Err(err).Msg("Cannot copy extApps.xml")
+			}
+			_ = os.Remove(aExtApps)
+			if err := os.Symlink(dExtApps, aExtApps); err != nil {
+				Log.Error().Err(err).Msg("Cannot create symlink to extApps.xml")
+			}
 		}
-		os.Remove(aConfCons)
-		if err := os.Symlink(dConfCons, aConfCons); err != nil {
-			Log.Error("Cannot create symlink to confCons.xml: ", err)
-		}
-	}
-	oldConfConsFiles, _ := filepath.Glob(PathJoin(Papp.AppPath, "confCons*"))
-	for _, oldConfConsFile := range oldConfConsFiles {
-		if err := os.Remove(oldConfConsFile); err != nil {
-			Log.Error("Cannot remove old confCons file: ", err)
-		}
-	}
 
-	// extApps.xml handling on close
-	if !Exists(dExtApps) && Exists(aExtApps) {
-		if err := CopyFile(aExtApps, dExtApps); err != nil {
-			Log.Error("Cannot copy extApps.xml: ", err)
+		// pnlLayout.xml handling on close
+		if !utl.Exists(dPnlLayout) && utl.Exists(aPnlLayout) {
+			if err := utl.CopyFile(aPnlLayout, dPnlLayout); err != nil {
+				Log.Error().Err(err).Msg("Cannot copy pnlLayout.xml")
+			}
+			_ = os.Remove(aPnlLayout)
+			if err := os.Symlink(dPnlLayout, aPnlLayout); err != nil {
+				Log.Error().Err(err).Msg("Cannot create symlink to pnlLayout.xml")
+			}
 		}
-		os.Remove(aExtApps)
-		if err := os.Symlink(dExtApps, aExtApps); err != nil {
-			Log.Error("Cannot create symlink to extApps.xml: ", err)
-		}
-	}
+	}()
 
-	// pnlLayout.xml handling on close
-	if !Exists(dPnlLayout) && Exists(aPnlLayout) {
-		if err := CopyFile(aPnlLayout, dPnlLayout); err != nil {
-			Log.Error("Cannot copy pnlLayout.xml: ", err)
-		}
-		os.Remove(aPnlLayout)
-		if err := os.Symlink(dPnlLayout, aPnlLayout); err != nil {
-			Log.Error("Cannot create symlink to pnlLayout.xml: ", err)
-		}
-	}
+	app.Launch(os.Args[1:])
 }
